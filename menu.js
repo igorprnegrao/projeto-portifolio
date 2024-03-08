b05ecb31aef3dc65eec1059d1339dd65e40364d5
Negrão, IPR <neurocientista@gmail.com>
2024-03-07 22:42:12 -0300

    .

--- menu.js
+++ menu.js
@@ -1,9 +1,11 @@
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu')
+let icon = document.getElementById('icone')
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
+    icon.classList.add('hidden')
 });
 
 menu.addEventListener('click', ()=>{
